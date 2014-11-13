cb0ca7c154bd622837b42b82ee55e14a92499ff4
TouNeko <touneko@gmail.com>
2014-11-14 03:45:45 +0800

    big updategit add --all

--- javascripts/all.js
+++ javascripts/all.js
@@ -3,8 +3,9 @@ $(".menu-btn").click(function(){
   $("body").toggleClass("open-menu");
 });
 
-// bs carousel
-$('.carousel').carousel();
+// appendaround
+$(".hero, .page-header").appendAround();
+
 
 // lightbox
 $('.shop-photo').magnificPopup({
@@ -12,3 +13,49 @@ $('.shop-photo').magnificPopup({
   removalDelay: 300,
   mainClass: 'mfp-fade'
 });
+
+// slick slider
+if ( $('#hero-slider').length != 0 ) {
+  $('#hero-slider').slick({
+    centerMode: true,
+    centerPadding: '0px',
+    slidesToShow: 1,
+    cssEase: 'ease-in-out',
+    autoplay: true,
+    autoplaySpeed: 5000,
+    speed: 1000,
+  });
+}
+
+if ( $('.photo-slider').length != 0 ) {
+   $('.photo-slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.photo-slider-nav'
+  });
+  $('.photo-slider-nav').slick({
+      slidesToShow: 7,
+      slidesToScroll: 1,
+      asNavFor: '.photo-slider-for',
+      dots: true,
+      centerMode: true,
+      focusOnSelect: true,
+      autoplay: true,
+      responsive: [
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 5,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3,
+        }
+      }
+    ]
+  });
+}
